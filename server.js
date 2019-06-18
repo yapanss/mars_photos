a93dde260088bad3b9e14f85ce471623747e4011
yapanss <yapanss@yahoo.fr>
2019-06-18 08:12:33 +0000

    Implemented request timeout amd other request errors

--- server.js
+++ server.js
@@ -14,14 +14,21 @@ app.get('/photos', (req, res) => {
   const camera = req.query.camera;
   const sol = req.query.sol;
   const photoUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&camera=${camera}&api_key=DEMO_KEY`;
-  //const photoUrl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY';
-  fetch(photoUrl)
-    .then(resp => resp.json())
+
+  fetch(photoUrl, {timeout: 5000})
+    .then(resp => {
+      return resp.json();
+    })
     .then(data => {
       console.log(data);
-      res.send(data);
+      res.json({success: true, data});
     })
-    .catch(err => console.log(err))
+    .catch(err => {
+      console.log(err);
+      const timeoutMessage = 'Error! your request timed out...';
+      const errorMessage = 'Oops! An error occured...';
+      res.json({success: false, err: err.type =='request-timeout'? timeoutMessage : errorMessage });
+    })
 });
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
