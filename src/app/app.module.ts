3b776dc754c00a47b3930b8e8360b98be06bfa76
yapanss <yapanss@yahoo.fr>
2019-06-18 02:51:21 +0000

    Added the form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { HeaderComponent } from './header/header.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 import { NgxMasonryModule } from 'ngx-masonry';
 
 import 'hammerjs';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PhotoListComponent,
-    HeaderComponent
+    HeaderComponent,
+    PhotoFormComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
+    FormsModule,
     MatToolbarModule,
+    MatCardModule,
     MatButtonModule,
     MatProgressSpinnerModule,
+    MatSelectModule,
+    MatInputModule,
+    MatIconModule,
     NgxMasonryModule
   ],
   providers: [],
